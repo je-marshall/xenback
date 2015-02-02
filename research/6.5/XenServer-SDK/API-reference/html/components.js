fee1d137c999d1a2ba34370f20101415fcd26dd8
Jon Marshall <jon@exactic.us>
2015-02-02 16:12:02 +0000

    Tidied up

--- research/6.5/XenServer-SDK/API-reference/html/components.js
+++
@@ -1,3 +0,0 @@
-executables = ['mpathalert', 'gen_api_main', 'xcp-rrdd', 'xn', 'perftest', 'xenopsd', 'xapi', 'xenopstest', 'list_domains', 'xe', 'block_device_io', 'datamodel', 'sm', 'squeezed', 'v6d', 'xcp-networkd', 'smtest', 'xiu']
-libraries = ['stats', 'server', 'xenops', 'client', 'common', 'vm_memory_constraints']
-packages = ['xapi-client']
